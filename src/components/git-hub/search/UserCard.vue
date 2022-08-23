49b3085ee77231d849243486c0cf864f16af1c15
ducnh <ducnhse20@gmail.com>
2022-08-23 09:40:58 +0700

    delete node_modules

--- src/components/git-hub/search/UserCard.vue
+++ src/components/git-hub/search/UserCard.vue
@@ -1,68 +1,74 @@
 <template>
   <div class="card-container p-2">
     <div class="row justify-between">
-      <div class="image col-4 rounded-lg">
-        <img
-          :src="
+      <div class="col-10 row" @click="goToDetailPage()">
+        <div class="image col-4 w-1/3 my-auto rounded-lg">
+          <img :src="
             user.avatar_url ||
             'https://bitsofco.de/content/images/2018/12/broken-1.png'
-          "
-          alt=""
-          class="rounded-lg"
-        />
-      </div>
-      <div class="user-info col-6 ml-3 py-2 font-jost overflow-hidden">
-        <span class="mb-2"
-          ><span class="font-bold">{{ route.query.name }}</span
-          >{{ userNameDisplay }}</span
-        >
-        <span class="text-xs">{{ `${follower} Followers` }}</span>
-        <span class="text-xs">{{ `${following} Followings` }}</span>
+          " alt="" class="rounded-lg" />
+        </div>
+        <div class="user-info col-6 ml-3 py-2 font-jost overflow-hidden">
+          <span class="mb-2 whitespace-nowrap"><span class="font-bold">{{ route.query.name }}</span>{{ userNameDisplay
+          }}</span>
+          <span class="text-xs">{{ follower || 'Loading ...' }}</span>
+          <span class="text-xs">{{ following || 'Loading ...' }}</span>
+        </div>
       </div>
       <div class="p-2 flex flex-col">
-        <span class="material-icons text-red text-sm"> favorite_border </span>
-        <span class="material-icons text-red text-sm"> favorite </span>
+        <span v-if="isFavorite" class="material-icons text-red text-sm" @click="onUnSetFavorite()"> favorite </span>
+        <span v-else class="material-icons text-red text-sm" @click="onSetFavorite()"> favorite_border </span>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import useFetchList from "@/api/user";
-import { computed, ref, watch } from "vue";
+import useFetchList, { IItemUser } from "@/api/user";
+import { computed, ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 const { fetchListUserFollows } = useFetchList();
 const props = defineProps({
   user: {
     type: Object,
-    default: () => {},
+    default: () => { },
+  },
+  isFavorite: {
+    type: Boolean,
+    default: false,
   },
 });
+const emit = defineEmits(['setFavorite', 'unSetFavorite', 'goToDetail'])
 const route = useRoute();
-const follower = ref(0);
-const following = ref(0);
+const follower = ref('');
+const following = ref('');
 const userNameDisplay = computed(() => {
-  return props.user.login.toLowerCase().replace(route.query.name, "");
+  return props.user.login.toLowerCase().replace(route.query.name?.toString().toLowerCase(), "");
 });
-watch(
-  () => props.user,
-  (value) => {
-    onGetUserFollows(value);
-  }
-);
-
+onMounted(() => {
+  onGetUserFollows(props.user);
+})
 const onGetUserFollows = async (user: any) => {
   const userUrls = {
     followers_url: user.followers_url,
     following_url: user.following_url.replace("{/other_user}", ""),
   };
   const totalFollowers =
-    (await fetchListUserFollows(userUrls.followers_url)) || 0;
+    (await fetchListUserFollows(userUrls.followers_url) || 0) + ' Followers';
   const totalFollowing =
-    (await fetchListUserFollows(userUrls.following_url)) || 0;
+    (await fetchListUserFollows(userUrls.following_url) || 0) + ' Followings';
   follower.value = totalFollowers;
   following.value = totalFollowing;
 };
+const onSetFavorite = () => {
+  emit('setFavorite', props.user)
+}
+const onUnSetFavorite = () => {
+  emit('unSetFavorite', props.user)
+}
+const goToDetailPage = () => {
+  emit('goToDetail', props.user)
+}
 </script>
 
 <style scoped lang="scss">
@@ -70,13 +76,16 @@ const onGetUserFollows = async (user: any) => {
   cursor: pointer;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
   border-radius: 10px;
+
   &:hover {
     transform: scale(1.1);
     transition: ease-in-out 0.2s;
   }
+
   .image {
-    width: 80px !important;
+    max-width: 80px !important;
   }
+
   .user-info {
     display: flex;
     flex-direction: column;
---
+++ src/pages/ghub/detail-page/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="detail">
+        <img src="@/assets/home-icon.svg" alt="" class="mb-5" />
+        <div class="user-info text-center">
+            <img :src="user.avatar_url" alt="" class="w-40 mx-auto rounded-full mb-2" />
+            <span class="font-bold text-xl font-arsenal">
+                {{ user.login }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import useStoreGitHub from '@/store/git-hub'
+const useStoreGHub = useStoreGitHub()
+const user = ref(useStoreGHub.userDetail)
+</script>
+
+<style scoped>
+</style>
